9a9c70e9c68ff872591120397e4cde90db0dd7f1
checko <checko@gmail.com>
2016-08-16 18:41:50 +0800

    add 2 fragments in main layout

---
+++ app/src/main/java/pp/checko/fragment/FragmentGame.java
@@ -0,0 +1,10 @@
+/**
+ * Created by charles-chang on 8/16/16.
+ */
+package pp.checko.fragment;
+
+import android.app.Fragment;
+
+public class FragmentGame extends Fragment{
+
+}
---
+++ app/src/main/java/pp/checko/fragment/FragmentResult.java
@@ -0,0 +1,8 @@
+package pp.checko.fragment;
+import android.app.Fragment;
+
+/**
+ * Created by charles-chang on 8/16/16.
+ */
+public class FragmentResult extends Fragment {
+}
