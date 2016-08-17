fbc4550ccf8f7d1118b7ed79e4ad4527e6c95e82
checko <checko@gmail.com>
2016-08-17 11:02:15 +0800

    add fragement layout files, and init function

--- app/src/main/java/pp/checko/fragment/FragmentResult.java
+++ app/src/main/java/pp/checko/fragment/FragmentResult.java
@@ -1,8 +1,17 @@
 package pp.checko.fragment;
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by charles-chang on 8/16/16.
  */
 public class FragmentResult extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragmentresult, container, false);
+    }
 }
